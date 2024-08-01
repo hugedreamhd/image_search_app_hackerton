5244741371f92de5cff4a5b4e19240e5e324c26d
Sangin Han <hugedreamhd@gmail.com>
2024-08-01 13:29:59 +0900

    feat: search input text 파일 생성 후 textField에 enable border, focusedborder를 이용 컬러와 폰트 적용

---
+++ lib/presentation/component/search_input_text.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_app_hackerton/presentation/ui/color_styles.dart';
+import 'package:image_search_app_hackerton/presentation/ui/text_styles.dart';
+
+class SearchInputText extends StatefulWidget {
+//data
+  final String inputTitle;
+  final String hintText;
+
+  const SearchInputText({
+    super.key,
+    required this.inputTitle,
+    required this.hintText,
+  });
+
+  @override
+  State<SearchInputText> createState() => _SearchInputTextState();
+}
+
+class _SearchInputTextState extends State<SearchInputText> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            hintStyle: TextStyles.smallTextRegular.copyWith(
+              color: ColorStyles.primary100,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                color: ColorStyles.primary100,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                color: ColorStyles.primary100,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
