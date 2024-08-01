b53aebbb4914a9d4bacf0a620246f58ecc4e0273
Sangin Han <hugedreamhd@gmail.com>
2024-08-01 17:31:49 +0900

    chore: repository_impl 작성

---
+++ lib/data/repository/pixabay_repository_impl.dart
@@ -0,0 +1,17 @@
+import 'package:image_search_app_hackerton/core/result.dart';
+import 'package:image_search_app_hackerton/data/data_source/pixabay_data_source.dart';
+import 'package:image_search_app_hackerton/data/mapper/pixabay_mapper.dart';
+import 'package:image_search_app_hackerton/domain/model/pixabay.dart';
+import 'package:image_search_app_hackerton/domain/repository/pixabay_repository.dart';
+
+class PixabayRepositoryImpl implements PixabayRepository {
+  final PixabayDataSource _pixabayDataSource;
+
+  PixabayRepositoryImpl(this._pixabayDataSource);
+
+  @override
+  Future<List<Pixabay>> getPixabay(String query) async {
+    final imageDto = await _pixabayDataSource.getImageResult(query);
+    return imageDto.map((e) => e.toPixabayDto()).toList();
+  }
+}
