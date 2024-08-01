a927726986f632839ca082a6d1d3f94266ea2b68
Sangin Han <hugedreamhd@gmail.com>
2024-08-01 17:28:20 +0900

    chore: id 추가

--- lib/data/mapper/pixabay_mapper.dart
+++ lib/data/mapper/pixabay_mapper.dart
@@ -1,9 +1,10 @@
 import 'package:image_search_app_hackerton/data/dto/pixabay_dto.dart';
-import 'package:image_search_app_hackerton/data/model/pixabay.dart';
+import 'package:image_search_app_hackerton/domain/model/pixabay.dart';
 
 extension DtoToModel on Hits {
-  Pixabay toImageItem() {
+  Pixabay toPixabayDto() {
     return Pixabay(
+      id: id ?? 0,
       ImageUrl: previewURL ?? '',
       tags: tags ?? '',
       user: user ?? '',
