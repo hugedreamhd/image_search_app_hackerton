3d930e073b739b1546bf8b9f4453d4f99dd60490
Sangin Han <hugedreamhd@gmail.com>
2024-08-02 09:48:47 +0900

    chore: textEdtingController로 제어 및 onChanged로 inputTitle콜백함수 호출 설정

--- lib/presentation/component/search_input_text.dart
+++ lib/presentation/component/search_input_text.dart
@@ -2,47 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:image_search_app_hackerton/presentation/ui/color_styles.dart';
 import 'package:image_search_app_hackerton/presentation/ui/text_styles.dart';
 
-class SearchInputText extends StatefulWidget {
+class SearchInputText extends StatelessWidget {
 //data
-  final String inputTitle;
+  final TextEditingController textEditingController;
+  final void Function(String) inputTitle;
   final String hintText;
 
   const SearchInputText({
     super.key,
     required this.inputTitle,
     required this.hintText,
+    required this.textEditingController,
   });
 
   @override
-  State<SearchInputText> createState() => _SearchInputTextState();
-}
-
-class _SearchInputTextState extends State<SearchInputText> {
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            hintText: widget.hintText,
-            hintStyle: TextStyles.smallTextRegular.copyWith(
-              color: ColorStyles.gray4,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: const BorderSide(
-                color: ColorStyles.primary100,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: const BorderSide(
-                color: ColorStyles.primary100,
-              ),
-            ),
+    return TextField(
+      controller: textEditingController,
+      onChanged: inputTitle,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyles.smallTextRegular.copyWith(
+          color: ColorStyles.gray4,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: ColorStyles.primary100,
           ),
         ),
-      ],
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: ColorStyles.primary100,
+          ),
+        ),
+      ),
     );
   }
 }
