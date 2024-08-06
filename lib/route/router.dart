84b8c5205a175dbec4bdd1b03e984b5106dc9c4e
Sangin Han <hugedreamhd@gmail.com>
2024-08-06 15:57:06 +0900

    chore: route 설정 변경

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,21 +1,27 @@
 import 'package:go_router/go_router.dart';
-import 'package:image_search_app_hackerton/domain/repository/pixabay_repository.dart';
 import 'package:image_search_app_hackerton/presentation/home/home_screen.dart';
-import 'package:image_search_app_hackerton/presentation/home/home_view_model.dart';
-import 'package:provider/provider.dart';
 
-final pixabayRepository = PixabayRepository();
+// PixabayDataSource 인스턴스 생성
+// final pixabayDataSource = PixabayDataSource();
+
+// PixabayRepository 인스턴스 생성
+// final PixabayRepository pixabayRepository =
+//     PixabayRepositoryImpl(pixabayDataSource);
 
 final router = GoRouter(
-
   initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => ChangeNotifierProvider(
-        create: (_) => HomeViewModel(pixabayRepository),
-        child: const HomeScreen(),
-      ),
+      builder: (context, state) => const HomeScreen(),
     ),
+
+    // GoRoute(
+    //   path: '/',
+    //   builder: (context, state) => ChangeNotifierProvider(
+    //     create: (_) => HomeViewModel(pixabayRepository),
+    //     child: const HomeScreen(),
+    //   ),
+    // ),
   ],
 );
